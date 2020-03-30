db204b643466f95b96184e8c51fc9577966c202d
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-03-30 10:20:33 +0200

    Upgrade react-native WIP

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import App from "./App";
 import { enableScreens } from "react-native-screens";
 import "./src/lib/error-tracking";
 
+require('react-native').unstable_enableLogBox()
+
 enableScreens();
 
 AppRegistry.registerComponent("HackerNeus", () => App);
